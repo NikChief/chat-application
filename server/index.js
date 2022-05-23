3aad6fe61540bdb743bd60970024092668c3fc3c
NikChief <niki3000-90@mail.ru>
2022-05-23 14:07:19 +0500

    creating route on server

--- server/index.js
+++ server/index.js
@@ -4,9 +4,13 @@ const http = require('http');
 
 const PORT = process.env.PORT || 4000;
 
+const router = require('./routes/main.routes')
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use('/',router);
 
 server.listen(PORT, () => {
   console.log('Server has started on', PORT);
