1c368f71bc8e95c53f4c05fd0ed50041f9d75982
NikChief <niki3000-90@mail.ru>
2022-05-23 19:40:36 +0500

    socket.on join on server index.js

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -18,8 +18,15 @@ function Chat() {
     setName(name);
     setRoom(room)
     
-    socket.emit('join', {name, room});
-    console.log(socket);
+    socket.emit('join', {name, room}, ()=>{
+      
+    });
+
+    return ()=> {
+      socket.emit('disconnect');
+      
+      socket.off();
+    }
   }, [ENDPOINT, location.search])
   return (
     <h1>Chat</h1>
