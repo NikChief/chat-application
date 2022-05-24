cdac0feaa816b1e0e66f53025e5ef5ecb779396d
NikChief <niki3000-90@mail.ru>
2022-05-24 08:03:07 +0500

    admin messages

--- server/index.js
+++ server/index.js
@@ -10,24 +10,37 @@ const PORT = process.env.PORT || 4000;
 const router = require('./routes/main.routes')
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server, { cors: {
-  origin: ['http://localhost:3000'],
-  methods: ['GET', 'POST'],
-}
+const io = socketio(server, {
+  cors: {
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST'],
+  }
 });
 
 io.on('connection', (socket) => {
 
-  socket.on('join', ({name, room}, callback)=>{
-    const { error, user } = addUser({ id: socket.id, name, room});
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
 
     if (error) return callback(error);
 
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to chat-room ${user.room}` });
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined! ` });
+
     socket.join(user.room);
-    
+
+    callback();
   })
 
-  socket.on('disconnect', ()=> {
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
     console.log('User had left chat.')
   })
 })
